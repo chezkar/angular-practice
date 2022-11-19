20df86ffc8844b877bf03f4a5d8a3b1d10207f1b
César Mayol <cesarmayol@gmail.com>
2022-11-19 16:42:44 -0600

    Fin sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,19 +10,34 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) {
     // console.log('Servicio activo')
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
-    //leer archivo
+  private cargarInfo(){
+    //leer archivo info
     this.http.get('assets/data/data-pagina.json')
       .subscribe(
           (resp: InfoPagina) => {
             this.cargada = true;
             this.info = resp;
+          }
+      );
+  }
+
+  private cargarEquipo(){
+    //leer archivo equpo
+    this.http.get('https://angular-html-c08d5-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe(
+          (resp: any) => {
+            this.cargada = true;
+            this.equipo = resp;
             console.log(resp);
           }
       );
-
-
   }
 }
